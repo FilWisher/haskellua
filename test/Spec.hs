8b70a3e321800341a15bb09c9fa571f0ab85f8ed
William Fisher <wfisher@cloudflare.com>
2017-07-21 10:45:05 +0100

    remove executable; adds initial test framework

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Lib
+import Test.HUnit
+
+firsttest = TestCase 
+  (assertEqual "are equal"
+    1 1)
+
+secondtest = TestCase 
+  (assertEqual "are not equal"
+    1 2)
+
+tests = test
+  [ TestLabel "first test" firsttest
+  , TestLabel "second test" secondtest
+  ]
+
+main :: IO Counts
+main = runTestTT tests
